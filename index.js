a15a4825d7dbe04d4526839320c10c4f1107cd38
Curtis Simpson <crsimpson5@gmail.com>
2020-08-23 13:44:24 -0500

    config

--- index.js
+++ index.js
@@ -7,18 +7,25 @@ const app = express();
 const apiRouter = require("./apiRouter");
 
 const PORT = process.env.PORT || 9000;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/disc-db"
 
 app.use(logger("dev"));
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("../build"));
+app.use("/api", apiRouter);
 
-mongoose.connect("mongodb://localhost/disc-db", { 
+mongoose.connect(MONGODB_URI, { 
   useNewUrlParser: true, 
   useUnifiedTopology: true 
 });
 
-app.use("/api", apiRouter);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}.`));
